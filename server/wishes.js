d890990ebd922ef7c2c09e75ba5a86a375ffa822
Khamarl C <khamarlchristie@hotmail.co.uk>
2022-09-27 14:41:24 +0100

    add wishes.js file

---
+++ server/wishes.js
@@ -0,0 +1,24 @@
+const wishes = [
+    {
+        id: 0,
+        wish: "I wish i was a little bit taller",
+        votes: 3,
+    },
+    {
+        id: 1,
+        wish: "I wish i was a baller ",
+        votes: 8,
+    },
+    {
+        id: 2,
+        wish: "I wish i had a rabbit in a hat",
+        votes: 2,
+    },
+    {
+        id: 3,
+        wish: "I wish I had 64 impala",
+        votes: 6,
+    }
+]
+
+module.exports = wishes;
