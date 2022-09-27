b63b101718a7352a47f0b57d344d4b8f0c36b3e8
Khamarl C <khamarlchristie@hotmail.co.uk>
2022-09-27 14:53:20 +0100

    add favourites get request

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
-const logRoute = require("./logRoute")
+const logRoute = require("./logRoute");
+const wishes = require("./wishes");
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,26 @@ app.use(logRoute);
 app.get("/" , (req, res) => {
 
     res.send("Welcome to the wishing well")
-})
+});
+
+app.get("/wishes", (req, res) => {
+    //gets wishes 
+    res.send(wishes)
+});
+
+app.get("/favourites", (req, res) => {
+
+    // sorts wishes by vote, highest to lowest 
+    const getFavourite = wishes.sort((a,b) => b.votes - a.votes)
+    res.send(getFavourite)
+});
+
+// app.get("/addwish", (req, res) => {
+
+//     //
+//     const wishData = req.body;
+
+// })
+
 
 module.exports = app;
